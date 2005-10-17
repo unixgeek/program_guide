0ad5b6f3f5bf56f5c985a8f2b4d97a7d1ca87f22
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-10-17 17:51:19 +0000

    Better RE for matching episodes. Better transformation of matching lines.

--- x-server_scripts/scrape.sh
+++ x-server_scripts/scrape.sh
@@ -1,5 +1,5 @@
 #
-# $Id: scrape.sh,v 1.3 2005-10-17 05:25:15 gunter Exp $
+# $Id: scrape.sh,v 1.4 2005-10-17 17:51:19 gunter Exp $
 #
 # requires: html2text sgrep
 #
@@ -12,10 +12,11 @@ fi
 
 ID="${1}"
 URL="${2}"
-HTML=`mktemp /tmp/scrape.html.XXXXXX`
+#HTML=`mktemp /tmp/scrape.html.XXXXXX`
+HTML=/tmp/scrape.html.VLJ0lp
 DATA=`mktemp /tmp/scrape.data.XXXXXX`
 
-fetch -q -o ${HTML} "${URL}"
+#fetch -q -o ${HTML} "${URL}"
 if [ "$?" -ne "0" ]; then
     echo "fetch failed."
     exit 1
@@ -33,19 +34,23 @@ echo "${PROGRAM} => ${HTML}"
 echo "${PROGRAM} => ${DATA}"
 
 # Possible line formats:
-# 1) X.|SEASON-EPISODE|PRODUCTION_CODE|DAY|MONTH|YEAR|TITLE
-# 2) X.|SEASON-EPISODE|PRODUCTION_CODE|TITLE
-# 3) X.|SEASON-EPISODE|DAY|MONTH|YEAR|TITLE
-# 4) X.|SEASON-EPISODE|TITLE
-for line in `html2text -nobs ${HTML} | tr -s ' ' | grep -e '^ [[:digit:]]' |  sed 's/^ //' | sed 's/- /-/' | tr ' ' '|'`
+#                 1|              2|  3|    4|   5|    6
+# 1) SEASON-EPISODE|PRODUCTION_CODE|DAY|MONTH|YEAR|TITLE
+#                 1|              2|   3|
+# 2) SEASON-EPISODE|PRODUCTION_CODE|TITLE
+#                 1|  2|    3|   4|   5|
+# 3) SEASON-EPISODE|DAY|MONTH|YEAR|TITLE
+#                 1|   2|
+# 4) SEASON-EPISODE|TITLE
+for line in `html2text -nobs ${HTML} | grep -e '[[:alnum:]]-[([:digit:]|[:space:][:digit:])]' | sed -E 's/^[[:space:]]+[[:digit:]]+\.//' | tr -s ' ' | sed 's/^ //' | sed 's/- /-/' | tr ' ' '|'`
 do
-    SEASON=`echo "${line}" | cut -d "|" -f 2 | cut -d "-" -f 1`
-    EPISODE=`echo "${line}" | cut -d "|" -f 2 | cut -d "-" -f 2`
-    PRODUCTION_CODE=`echo "${line}" | cut -d "|" -f 3`
-    DAY=`echo "${line}" | cut -d "|" -f 4`
-    MONTH=`echo "${line}" | cut -d "|" -f 5`
-    YEAR=`echo "${line}" | cut -d "|" -f 6`
-    TITLE=`echo "${line}" | cut -d "|" -f 7 | tr '_' ' ' | sed "s/'/''/g"`
+    SEASON=`echo "${line}" | cut -d "|" -f 1 | cut -d "-" -f 1`
+    EPISODE=`echo "${line}" | cut -d "|" -f 1 | cut -d "-" -f 2`
+    PRODUCTION_CODE=`echo "${line}" | cut -d "|" -f 2`
+    DAY=`echo "${line}" | cut -d "|" -f 3`
+    MONTH=`echo "${line}" | cut -d "|" -f 4`
+    YEAR=`echo "${line}" | cut -d "|" -f 5`
+    TITLE=`echo "${line}" | cut -d "|" -f 6 | tr '_' ' '`
     AIR_DATE=`date -j -f %y%b%d ${YEAR}${MONTH}${DAY} +%y%m%d 2> /dev/null`
     CASE=1
 
@@ -54,24 +59,24 @@ do
     if [ -z "${DAY}" ] && [ -z "${TITLE}" ]; then
         PRODUCTION_CODE=null
         AIR_DATE=null
-        TITLE=`echo "${line}" | cut -d "|" -f 3`
+        TITLE=`echo "${line}" | cut -d "|" -f 2`
         CASE=4
     # If TITLE is empty, then PRODUCTION_CODE or AIR_DATE is missing from 
     # the data.
     elif [ -z "${TITLE}" ]; then
         # Try processing a date. If date is successful, the PRODUCITON_CODE
         # is the missing field; otherwise, AIR_DATE is missing.
-        DAY=`echo "${line}" | cut -d "|" -f 3`
-        MONTH=`echo "${line}" | cut -d "|" -f 4`
-        YEAR=`echo "${line}" | cut -d "|" -f 5`
+        DAY=`echo "${line}" | cut -d "|" -f 2`
+        MONTH=`echo "${line}" | cut -d "|" -f 3`
+        YEAR=`echo "${line}" | cut -d "|" -f 4`
         AIR_DATE=`date -j -f %y%b%d ${YEAR}${MONTH}${DAY} +%y%m%d 2> /dev/null`
         if [ "$?" -ne "0" ]; then
             AIR_DATE=null
-            TITLE=`echo "${line}" | cut -d "|" -f 4 | tr '_' ' ' | sed "s/'/''/g"`
+            TITLE=`echo "${line}" | cut -d "|" -f 3 | tr '_' ' '`
             CASE=2
         else
             PRODUCTION_CODE=null
-            TITLE=`echo "${line}" | cut -d "|" -f 6 | tr '_' ' ' | sed "s/'/''/g"`
+            TITLE=`echo "${line}" | cut -d "|" -f 5 | tr '_' ' '`
             CASE=3
         fi
     fi
