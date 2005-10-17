ebc52fd5e16f84c6c3b36acc54e73de737fd5fc0
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-10-17 17:49:53 +0000

    Update programs that have never been updated before regardless of what the do_update field says. Don't attempt to update a program with a null url.

--- x-server_scripts/update_programs.sh
+++ x-server_scripts/update_programs.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #
-# $Id: update_programs.sh,v 1.1 2005-10-17 05:25:55 gunter Exp $
+# $Id: update_programs.sh,v 1.2 2005-10-17 17:49:53 gunter Exp $
 #
 . program_guide.config
 
 SQL=\
 "SELECT id, name, url
 FROM program
-WHERE do_update = 1"
+WHERE (do_update = 1
+       OR last_update is NULL)
+AND url is NOT NULL"
 
 RESULTS=`mysql -s --skip-column-names ${DATABASE} -e "${SQL}" | tr '\t' '|' | tr ' ' '_'`
 
