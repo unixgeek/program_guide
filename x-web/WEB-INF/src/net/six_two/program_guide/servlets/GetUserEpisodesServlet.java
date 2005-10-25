2ff98831268ecdf44a1db7730d66c3f22cc55de2
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-10-25 13:55:08 +0000

    Call the jsp. Moved post to different servlet.

--- x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserEpisodesServlet.java
+++ x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserEpisodesServlet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: GetUserEpisodesServlet.java,v 1.4 2005-10-24 22:45:06 gunter Exp $
+ * $Id: GetUserEpisodesServlet.java,v 1.5 2005-10-25 13:55:08 gunter Exp $
  */
 package net.six_two.program_guide.servlets;
 
@@ -10,6 +10,7 @@ import java.util.Enumeration;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -43,69 +44,23 @@ public class GetUserEpisodesServlet extends HttpServlet {
                 selectAllEpisodesForUser(connection, user, program);
             
             connection.close();
-            
+            log(program.getName());
             request.setAttribute("userEpisodesList", userEpisodes);
             request.setAttribute("program", program);
-        } catch (NamingException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    protected void doPost(HttpServletRequest request,
-            HttpServletResponse response) throws IOException,
-            ServletException {
-        try {
-            InitialContext context = new InitialContext();
-            DataSource source = (DataSource) 
-                context.lookup("java:comp/env/jdbc/program_guide");
-            Connection connection = source.getConnection();
-            
-            User user = Persistor.selectUser(connection, "gunter");
-            UserManager.authenticateUser(user, "");
-            
-            int program_id = 
-                Integer.parseInt(request.getParameter("program_id"));
-            Program program = Persistor.selectProgram(connection, program_id);
-            
-            /*UserEpisode[] userEpisodes = Persistor.
-                selectAllEpisodesForUser(connection, user, program);*/
-            Persistor.deleteQueuedForUser(connection, user, program);
-            
-            String[] queued = request.getParameterValues("queued");
-            if (queued != null) {
-                for (int i = 0; i != queued.length; i++) {
-                    String tokens[] = queued[i].split("_");
-                    Episode episode = new Episode();
-                    episode.setProgramId(program_id);
-                    episode.setSeason(tokens[0].charAt(0));
-                    episode.setNumber(Integer.parseInt(tokens[1]));
-                    
-                    Persistor.insertQueuedForUser(connection, user, episode);
-                }
-            }
-            
-            Persistor.deleteViewedForUser(connection, user, program);
-            
-            String[] viewed = request.getParameterValues("viewed");
-            if (viewed != null) {
-                for (int i = 0; i != viewed.length; i++) {
-                    String tokens[] = viewed[i].split("_");
-                    Episode episode = new Episode();
-                    episode.setProgramId(program_id);
-                    episode.setSeason(tokens[0].charAt(0));
-                    episode.setNumber(Integer.parseInt(tokens[1]));
-                    
-                    Persistor.insertViewedForUser(connection, user, episode);
-                }
-            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("episodes.jsp");
+            dispatcher.forward(request, response);
         } catch (NamingException e) {
             e.printStackTrace();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         
+
+    }
+    
+    protected void doPost(HttpServletRequest request, 
+            HttpServletResponse response) throws IOException, 
+            ServletException {
         doGet(request, response);
     }
 }
