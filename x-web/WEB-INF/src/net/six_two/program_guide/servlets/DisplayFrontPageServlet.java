6131c0261843828bc816a46fbd8d167f9e9c3a7d
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-10-26 03:20:40 +0000

    Decide what to display first.

---
+++ x-web/WEB-INF/src/net/six_two/program_guide/servlets/DisplayFrontPageServlet.java
@@ -0,0 +1,48 @@
+/*
+ * $Id: DisplayFrontPageServlet.java,v 1.1 2005-10-26 03:20:40 gunter Exp $
+ */
+package net.six_two.program_guide.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import net.six_two.program_guide.Persistor;
+import net.six_two.program_guide.UserManager;
+import net.six_two.program_guide.tables.User;
+
+public class DisplayFrontPageServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, 
+            HttpServletResponse response) throws IOException, 
+            ServletException {
+   
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+            return;
+        }
+        
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+            return;
+        }
+            
+        session.setAttribute("user", user);
+        request.setAttribute("user", user);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("GetUserRecentEpisodes.do");
+        dispatcher.forward(request, response);
+    }
+}
