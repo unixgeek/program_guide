c8072293825adc0f76342e35926c2bf3f4d58f73
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-10-29 18:41:03 +0000

    Extend GenericServlet. Get previous 6 days and the next 6 days episodes.

--- x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserRecentEpisodesServlet.java
+++ x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserRecentEpisodesServlet.java
@@ -1,73 +1,56 @@
 /*
- * $Id: GetUserRecentEpisodesServlet.java,v 1.2 2005-10-29 00:59:41 gunter Exp $
+ * $Id: GetUserRecentEpisodesServlet.java,v 1.3 2005-10-29 18:41:03 gunter Exp $
  */
 package net.six_two.program_guide.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Enumeration;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import javax.sql.DataSource;
 
 import net.six_two.program_guide.Persistor;
-import net.six_two.program_guide.UserManager;
-import net.six_two.program_guide.tables.Episode;
-import net.six_two.program_guide.tables.Program;
 import net.six_two.program_guide.tables.User;
 import net.six_two.program_guide.tables.UserEpisode;
 
-public class GetUserRecentEpisodesServlet extends HttpServlet {
+public class GetUserRecentEpisodesServlet extends GenericServlet {
     protected void doGet(HttpServletRequest request, 
             HttpServletResponse response) throws IOException, 
             ServletException {
 
-        HttpSession session = request.getSession(false);
-        if (session == null) {
-            request.setAttribute("message", "You must login first.");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-            dispatcher.forward(request, response);
+        User user = getUserFromRequest(request);
+        if (user == null) {
+            redirectLogin(request, response);
             return;
         }
         
-        User user = (User) session.getAttribute("user");
-        if (user == null) {
-            request.setAttribute("message", "You must login first.");
-            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-            dispatcher.forward(request, response);
+        Connection connection = getConnection();
+        if (connection == null) {
+            redirectError(request, response, 
+                    "Couldn't connect to the database.");
             return;
         }
                 
         try {
-            InitialContext context = new InitialContext();
-            DataSource source = (DataSource) 
-            context.lookup("java:comp/env/jdbc/program_guide");
-            Connection connection = source.getConnection();
+            UserEpisode[] previousEpisodes = Persistor.
+                selectAllEpisodesForUser(connection, user, -6);
             
-            
-            
-            // Between next week and last week.
-            UserEpisode[] userEpisodes = Persistor.
-            selectAllEpisodesForUser(connection, user, 6);
+            UserEpisode[] nextEpisodes = Persistor.
+                selectAllEpisodesForUser(connection, user, 6);
             
             connection.close();
             
-            request.setAttribute("userEpisodesList", userEpisodes);
-            RequestDispatcher dispatcher = request.getRequestDispatcher("recent.jsp");
-            dispatcher.forward(request, response);
-        } catch (NamingException e) {
-            e.printStackTrace();
+            request.setAttribute("nextEpisodesList", nextEpisodes);
+            request.setAttribute("previousEpisodesList", previousEpisodes);
         } catch (SQLException e) {
-            e.printStackTrace();
+            redirectError(request, response, e.getMessage());
         }
+        
+        RequestDispatcher dispatcher = request.getRequestDispatcher("recent.jsp");
+        dispatcher.forward(request, response);
     }
     
     protected void doPost(HttpServletRequest request, 
