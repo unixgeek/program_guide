f6990a7d1dd4b454c0e12794feb79573420fa856
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-11-01 15:42:06 +0000

    Show what changed in the episode table. (Untested)

--- x-server_scripts/update_programs.sh
+++ x-server_scripts/update_programs.sh
@@ -1,17 +1,28 @@
 #!/bin/sh
 #
-# $Id: update_programs.sh,v 1.5 2005-10-27 15:48:21 gunter Exp $
+# $Id: update_programs.sh,v 1.6 2005-11-01 15:42:06 gunter Exp $
 #
 . program_guide.config
 
-SQL=\
+BEFORE=`mktemp /tmp/update_programs.before.XXXXXX`
+AFTER=`mktemp /tmp/update_programs.after.XXXXXX`
+
+EPISODE_SQL=\
+"SELECT *
+FROM episode
+ORDER BY program_id, season, number"
+
+mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} --skip-column-names ${DATABASE} \
+    -e "${EPISODE_SQL}" > ${BEFORE}
+
+PROGRAM_SQL=\
 "SELECT id, name, url
 FROM program
 WHERE (do_update = 1
        OR last_update is NULL)
 AND url is NOT NULL"
 
-RESULTS=`mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} -s --skip-column-names ${DATABASE} -e "${SQL}" | tr '\t' '|' | tr ' ' '_'`
+RESULTS=`mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} -s --skip-column-names ${DATABASE} -e "${PROGRAM_SQL}" | tr '\t' '|' | tr ' ' '_'`
 
 for result in ${RESULTS}
 do
@@ -21,5 +32,12 @@ do
     echo "${NAME} => ./scrape ${ID} ${NAME} ${URL}"
     ./scrape.sh "${ID}" "${NAME}" "${URL}"
 done
+
+mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} --skip-column-names ${DATABASE} \
+    -e "${EPISODE_SQL}" > ${AFTER}
+
+diff -u ${BEFORE} ${AFTER}
+
+rm -f ${BEFORE} ${AFTER}
 
 exit 0
