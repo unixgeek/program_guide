e10f296c1b1baf9d747421b7332f7954eb890ae4
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-11-03 16:53:17 +0000

    Dropped queued and viewed. Created status to hold queued or viewed which are now mutually exclusive.

--- x-database/program_guide.sql
+++ x-database/program_guide.sql
@@ -10,7 +10,7 @@
 
 CREATE TABLE episode (
   program_id int(11) unsigned NOT NULL default '0',
-  season varchar(2) NOT NULL default '',
+  season char(2) NOT NULL default '',
   number smallint(6) unsigned NOT NULL default '0',
   production_code varchar(32) default NULL,
   original_air_date date default NULL,
@@ -33,14 +33,15 @@ CREATE TABLE program (
 ) TYPE=MyISAM;
 
 --
--- Table structure for table `queued`
+-- Table structure for table `status`
 --
 
-CREATE TABLE queued (
+CREATE TABLE status (
   user_id int(11) NOT NULL default '0',
   program_id int(11) unsigned NOT NULL default '0',
-  season varchar(2) NOT NULL default '0',
+  season char(2) NOT NULL default '0',
   episode_number int(11) unsigned NOT NULL default '0',
+  status enum('queued','viewed') NOT NULL default 'queued',
   PRIMARY KEY  (user_id,program_id,season,episode_number)
 ) TYPE=MyISAM;
 
@@ -67,17 +68,5 @@ CREATE TABLE user (
   level tinyint(3) unsigned NOT NULL default '1',
   PRIMARY KEY  (id),
   UNIQUE KEY username (username)
-) TYPE=MyISAM;
-
---
--- Table structure for table `viewed`
---
-
-CREATE TABLE viewed (
-  user_id int(11) NOT NULL default '0',
-  program_id int(11) unsigned NOT NULL default '0',
-  season varchar(2) NOT NULL default '',
-  episode_number int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY  (user_id,program_id,season,episode_number)
 ) TYPE=MyISAM;
 
