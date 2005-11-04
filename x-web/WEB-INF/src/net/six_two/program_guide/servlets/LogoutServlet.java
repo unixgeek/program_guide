c23b608a3a28a235101dd1f0e88fcac6f520cfb2
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2005-11-04 04:23:19 +0000

    Removed unused imports. Removed unused user. Applied proper formatting.

--- x-web/WEB-INF/src/net/six_two/program_guide/servlets/LogoutServlet.java
+++ x-web/WEB-INF/src/net/six_two/program_guide/servlets/LogoutServlet.java
@@ -1,5 +1,5 @@
 /*
- * $Id: LogoutServlet.java,v 1.3 2005-11-01 03:46:45 gunter Exp $
+ * $Id: LogoutServlet.java,v 1.4 2005-11-04 04:23:19 gunter Exp $
  */
 package net.six_two.program_guide.servlets;
 
@@ -10,21 +10,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.six_two.program_guide.tables.User;
-
 public class LogoutServlet extends GenericServlet {
     protected void doGet(HttpServletRequest request, 
             HttpServletResponse response) throws IOException, 
             ServletException {
         
-        User user = getUserFromRequest(request);
-            request.getSession().invalidate();
-            /*
-             * Don't use redirecLogin()--it's intended for redirection from
-             * attempts to access pages that require authorization.  
-             */ 
-            RequestDispatcher dispatcher = request.getRequestDispatcher("DisplayFrontPage.do");
-            dispatcher.forward(request, response);
-            return;
+        request.getSession().invalidate();
+        /*
+         * Don't use redirecLogin()--it's intended for redirection from
+         * attempts to access pages that require authorization.  
+         */ 
+        RequestDispatcher dispatcher = request.getRequestDispatcher("DisplayFrontPage.do");
+        dispatcher.forward(request, response);
+        return;
     }
 }
