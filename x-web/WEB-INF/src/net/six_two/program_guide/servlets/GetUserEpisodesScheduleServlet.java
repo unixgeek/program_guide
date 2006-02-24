307d29b5c21f2364d055cec65982ca0af10f58d3
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2006-02-24 23:46:02 +0000

    Put all episodes in one list. Make one call to the persistence layer for the episodes. Added attributes for from date and to date.

--- x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserEpisodesScheduleServlet.java
+++ x-web/WEB-INF/src/net/six_two/program_guide/servlets/GetUserEpisodesScheduleServlet.java
@@ -1,18 +1,22 @@
 /*
- * $Id: GetUserEpisodesScheduleServlet.java,v 1.7 2005-11-11 16:37:45 gunter Exp $
+ * $Id: GetUserEpisodesScheduleServlet.java,v 1.8 2006-02-24 23:46:02 gunter Exp $
  */
 package net.six_two.program_guide.servlets;
 
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.six_two.misc.Time;
 import net.six_two.program_guide.Persistor;
+import net.six_two.program_guide.UserEpisodeForSchedule;
 import net.six_two.program_guide.tables.TorrentSite;
 import net.six_two.program_guide.tables.User;
 import net.six_two.program_guide.tables.UserEpisode;
@@ -36,35 +40,33 @@ public class GetUserEpisodesScheduleServlet extends GenericServlet {
         }
                 
         try {
-            UserEpisode[] todaysEpisodes = Persistor.
-                selectAllEpisodesForUser(connection, user, 0, 0);
-            
-            UserEpisode[] previousEpisodes = Persistor.
-                selectAllEpisodesForUser(connection, user, -1, -6);
-            
-            UserEpisode[] nextEpisodes = Persistor.
-                selectAllEpisodesForUser(connection, user, 1, 6);
+            UserEpisode[] episodes = Persistor.
+                selectAllEpisodesForUser(connection, user, -6, 6);
             
             TorrentSite site = Persistor.selectTorrentSite(connection);
             
             connection.close();
            
-            for (int i = 0; i != todaysEpisodes.length; i++)
-                todaysEpisodes[i].getEpisode().setTitle(
-                    filterContent(todaysEpisodes[i].getEpisode().getTitle()));
+            UserEpisodeForSchedule[] episodesSchedule = 
+                new UserEpisodeForSchedule[episodes.length];
+            
+            for (int i = 0; i != episodes.length; i++) {
+                episodes[i].getEpisode().setTitle(
+                    filterContent(episodes[i].getEpisode().getTitle()));
+                episodesSchedule[i] = new UserEpisodeForSchedule(episodes[i]);
+            }
 
-            for (int i = 0; i != previousEpisodes.length; i++)
-                previousEpisodes[i].getEpisode().setTitle(
-                    filterContent(
-                            previousEpisodes[i].getEpisode().getTitle()));
-
-            for (int i = 0; i != nextEpisodes.length; i++)
-                nextEpisodes[i].getEpisode().setTitle(
-                    filterContent(nextEpisodes[i].getEpisode().getTitle()));
-
-            request.setAttribute("todaysEpisodesList", todaysEpisodes);
-            request.setAttribute("nextEpisodesList", nextEpisodes);
-            request.setAttribute("previousEpisodesList", previousEpisodes);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(Time.datePart(
+                    new Date(System.currentTimeMillis())));
+            calendar.add(Calendar.DATE, -6);
+            Date fromDate = calendar.getTime();
+            calendar.add(Calendar.DATE, 12);
+            Date toDate = calendar.getTime();
+            
+            request.setAttribute("fromDate", fromDate);
+            request.setAttribute("toDate", toDate);
+            request.setAttribute("episodesList", episodesSchedule);
             request.setAttribute("site", site);
         } catch (SQLException e) {
             redirectError(request, response, e.getMessage());
