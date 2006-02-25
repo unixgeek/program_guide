99f0887ad9850ad5326cbc48154553eedab9ffc3
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2006-02-25 03:37:54 +0000

    Removed input formats as they aren't needed, moron.

--- x-web/WEB-INF/src/net/six_two/program_guide/ContextListener.java
+++ x-web/WEB-INF/src/net/six_two/program_guide/ContextListener.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ContextListener.java,v 1.2 2006-02-22 06:32:07 gunter Exp $
+ * $Id: ContextListener.java,v 1.3 2006-02-25 03:37:54 gunter Exp $
  */
 package net.six_two.program_guide;
 
@@ -21,9 +21,8 @@ public class ContextListener implements ServletContextListener {
         context.setAttribute("packageName", p.getName());
         context.setAttribute("packageVersion", p.getVersion());
         context.setAttribute("dateDisplayFormat", p.getDateDisplayFormat());
-        context.setAttribute("dateInputFormat", "yyyy-MM-dd");
         context.setAttribute("timestampDisplayFormat", 
                 p.getTimestampDisplayFormat());
-        context.setAttribute("timestampInputFormat", "yyyy-MM-dd hh:mm:ss.S");
+
     }
 }
