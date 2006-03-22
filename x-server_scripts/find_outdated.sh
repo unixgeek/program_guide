bd911696923ad8dcc4a2e7c98bdbff1394462fe4
Gunter Wambaugh <gunter.wambaugh@gmail.com>
2006-03-22 04:56:13 +0000

    Use MySQL to format dates. Don't log directly to the database.

--- x-server_scripts/find_outdated.sh
+++ x-server_scripts/find_outdated.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# $Id: find_outdated.sh,v 1.2 2006-01-14 22:50:12 gunter Exp $
+# $Id: find_outdated.sh,v 1.3 2006-03-22 04:56:13 gunter Exp $
 #
 # requires: lynx
 #
@@ -11,10 +11,8 @@
 #
 # This script ignores everything after the year and assumes the time is midnight.
 # In other words: it's a date, not a timestamp.
-. ${HOME}/.program_guide.conf
 
-LOG=`mktemp /tmp/find_outdated.log.XXXXXX`
-LOG_INSERT=`mktemp /tmp/find_outdate.insert.XXXXXX` 
+. program_guide.conf
 
 SQL=\
 "SELECT id, name, last_update, url
@@ -35,34 +33,36 @@ do
     LAST_UPDATE="`echo ${i} | cut -d "|" -f 3`"
     URL="`echo ${i} | cut -d "|" -f 4`"
 
-    LAST_UPDATE_MS="`date -j -f %Y-%m-%d_%H:%M:%S "${LAST_UPDATE}" '+%s'`"
-
     CURRENT_TIMESTAMP="`lynx -nolist -dont_wrap_pre -dump "${URL}" | \
         egrep -e '[[:alpha:]]{3}, [[:digit:]]{2} [[:alpha:]]{3} [[:digit:]]{4} [[:digit:]]{2}:[[:digit:]]{2}' | \
         sed 's/ *//' | sed 's/ Cast Photo$//' | sed -E 's/[[:digit:]]{2}:[[:digit:]]{2}.*/00:00:00/'`"
-    CURRENT_TIMESTAMP_MS="`date -j -f '%a, %d %b %Y %H:%M:%S' "${CURRENT_TIMESTAMP}" '+%s'`"
 
-    if [ -z "${CURRENT_TIMESTAMP_MS}" ]; then
-        echo "No date for ${NAME} (${ID})" >> ${LOG} 2>&1
+    if [ -z "${CURRENT_TIMESTAMP}" ]; then
+        echo "No date for ${NAME} (${ID})"
         continue
     fi
 
-    if [ "${LAST_UPDATE_MS}" -lt "${CURRENT_TIMESTAMP_MS}" ]; then
-        echo -n "[U] " >> ${LOG} 2>&1
+SQL=\
+"SELECT 1       
+FROM program
+WHERE id = ${ID}
+AND STR_TO_DATE('${CURRENT_TIMESTAMP}', '%a, %d %b %Y %k:%i') > last_update"
+
+    RESULT=`mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} -s --skip-column-names ${DATABASE} -e "${SQL}"`
+    
+    if [ -z "${RESULT}" ]; then
+        RESULT="0"
+    fi
+    
+    if [ "${RESULT}" -eq "1" ]; then
+        echo -n "[U] "
         mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} ${DATABASE} -e "UPDATE program SET last_update = null WHERE ID = ${ID}"
     else
-        echo -n "[S] " >> ${LOG} 2>&1
+        echo -n "[S] "
     fi
-    echo "${NAME}\t\t(`date -j -f '%s' ${LAST_UPDATE_MS}`\t`date -j -f '%s' ${CURRENT_TIMESTAMP_MS}`)" >> ${LOG} 2>&1
+    
+    echo "${NAME}\t\t(${LAST_UPDATE}\t${CURRENT_TIMESTAMP})"
 done
-
-echo -n "INSERT INTO log VALUES (null, 'find_outdated.sh', CURRENT_TIMESTAMP(), '" > ${LOG_INSERT}
-cat ${LOG} | sed "s/'/''/g" >> ${LOG_INSERT}
-echo "');" >> ${LOG_INSERT}
-
-mysql -u ${MYSQLUSER} -p${MYSQLPASSWORD} ${DATABASE} < ${LOG_INSERT}
-
-rm -f ${LOG} ${LOG_INSERT}
 
 exit 0
 
